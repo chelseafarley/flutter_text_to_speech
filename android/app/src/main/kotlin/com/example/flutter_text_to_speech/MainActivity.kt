058635c3e1acba7a69fd32a5f9aaff4377672f74
Chelsea Farley <chelsea@tripwiretech.com>
2022-08-06 11:32:27 +1200

    tts flutter

---
+++ android/app/src/main/kotlin/com/example/flutter_text_to_speech/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_text_to_speech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+enum TtsState { playing, stopped }
+
+class _MyAppState extends State<MyApp> {
+  late FlutterTts _flutterTts;
+  String? _tts;
+  TtsState _ttsState = TtsState.stopped;
+
+  @override
+  void initState() {
+    super.initState();
+    initTts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _flutterTts.stop();
+  }
+
+  initTts() async {
+    _flutterTts = FlutterTts();
+    await _flutterTts.awaitSpeakCompletion(true);
+
+    _flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        _ttsState = TtsState.playing;
+      });
+    });
+
+    _flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        _ttsState = TtsState.stopped;
+      });
+    });
+
+    _flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        _ttsState = TtsState.stopped;
+      });
+    });
+
+    _flutterTts.setErrorHandler((message) {
+      setState(() {
+        print("Error: $message");
+        _ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Flutter TTS')),
+            body: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(children: [input(), button()]),
+            )));
+  }
+
+  Widget input() => Container(
+        alignment: Alignment.topCenter,
+        padding: const EdgeInsets.all(25.0),
+        child: TextField(
+          onChanged: (String value) {
+            setState(() {
+              _tts = value;
+            });
+          },
+        ),
+      );
+
+  Widget button() {
+    if (_ttsState == TtsState.stopped) {
+      return TextButton(onPressed: speak, child: const Text('Play'));
+    } else {
+      return TextButton(onPressed: stop, child: const Text('Stop'));
+    }
+  }
+
+  Future speak() async {
+    await _flutterTts.setVolume(1);
+    await _flutterTts.setSpeechRate(0.5);
+    await _flutterTts.setPitch(1);
+
+    if (_tts != null) {
+      if (_tts!.isNotEmpty) {
+        await _flutterTts.speak(_tts!);
+      }
+    }
+  }
+
+  Future stop() async {
+    var result = await _flutterTts.stop();
+    if (result == 1) {
+      setState(() {
+        _ttsState = TtsState.stopped;
+      });
+    }
+  }
+}
